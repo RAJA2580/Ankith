ac03b2b3a2212a1ef797df842dcf40893234322f
RAJA2580 <somurajashekar@gmail.com>
2021-03-01 15:22:24 +0530

    how r u

---
+++ config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_963loyn7H', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5gjih2c20f6p26uiljdipaqvop', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
